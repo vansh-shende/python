f827a1c49958fac2efafbefb009c6ab225b3f0ba
vansh-shende <vshende719@gmail.com>
2025-04-17 10:02:01 +0000

    circle

--- fitting.py
+++ fitting.py
@@ -1,25 +1,14 @@
-import numpy as np
 import matplotlib.pyplot as plt
-### x and y values ###
-
-x = np.array([1,2,3,4,5,6,7,8,9,10])
-y = np.array([2.2 , 2.8 , 3.6 , 4.5 , 5.1 , 6.2 , 6.8 , 8.1 , 9 , 9.9])
-
-# fit a line y = ax+b using numpy's polyfit
-
-a,b = np.polyfit(x,y,1) #1 is linear
-
-#plot original scatter points 
-plt.scatter(x,y,color='red',label="sample Data")
-
-# generate y-values for the fitted line
-y_fit=a*x+b
-
-#plot best.fit line
-plt.plot(x,y_fit, color='blue', label=f"best fit line:y={a:.2f}x+{b:.2f}")
-plt.xlabel('x-axis"')
-plt.ylabel('y-axis"')
-plt.title("line fitting using least square")
-plt.legend()
+fig, ax = plt.subplots()
+circle = plt.circle((0,0), radius=5, color= 'blue', fill = 'false')
+ax.add_patch(circle)
+ax.set_xlim(-6,6)
+ax.set_ylim(-6,6)
+ax.set.xlabel("x-axis")
+ax.set_ylabel("y-axis")
+ax.set_title("circle using plt.circle()")
+ax.axhline(0 , color= 'black' , linewidth = 1)
+ax.axvline(0 , color= 'black' , linewidth = 1)
+ax.set_aspect('equal')
 plt.grid()
 plt.show()
